31f6bd14d4443dd35a80ebc31b50a4a2668e5d2a
ywettai_dev <ywettai.dev@gmail.com>
2020-01-23 09:58:21 +0630

    update App.js

--- functional-react/src/App.js
+++ functional-react/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Item = (props) => (
+  <li>{props.name}</li>
+);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <Item name="Apple" />
+      <Item name="Orange" />
+      <Item name="Banana" />
+    </ul>
   );
-}
+};
 
 export default App;
